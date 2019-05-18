d8adec3747e1bae9abce9e5d5a93a6df7990ee3d
Tara Suan <suant@uci.edu>
2019-05-17 22:35:02 -0700

    in process chat functionality; debugging

--- public/js/main.js
+++ public/js/main.js
@@ -1,20 +1,61 @@
 /* functions for general use */
 /* This function returns the value associated with 'whichParam' on the URL */
 
-function getURLParameters(whichParam) {
-  var pageURL = window.location.search.substring(1);
+function getURLParameters(whichParam) {   
+  var pageURL = window.location.search.substring(1);   
   var pageURLVariables = pageURL.split('&');
-  for(var i = 0; i < pageURLVariables.length; i++){
-    var parameterName = pageURLVariables[i].split('=');
-    if(parameterName[0] == whichParam) {
-      return parameterName[1];
-    }
-  }
+  for(var i = 0; i < pageURLVariables.length;i++){
+      var parameterName = pageURLVariables[i].split('=');
+      if(parameterName[0] == whichParam) {
+             return parameterName[1];
+      }
+  } 
 }
 
 var username = getURLParameters('username');
 if('undefined' == typeof username || !username) {
-  username = 'Anonymous_'+Math.random();
+   username = 'Anonymous_'+Math.random(); 
+ }
+
+var chat_room = 'One_Room';
+
+/* connect to the  socket server */
+var socket = io.connect();
+
+
+socket.on('log', function(array){
+  console.log.apply(console,array);
+});
+
+socket.on('join_room_response', function(payload){
+  if(payload.result == 'fail') {
+    alert(payload.message);
+    return;
+  }
+  $('#messages').append('<p>New user joined the room: '+payload.username+'</p>');
+});
+
+function send_message() {
+  var payload = {};
+  payload.room = chat_room;
+  payload.username = username;
+  payload.message = $('#send_message_holder')val();
+  console.log('*** Client Log Message: \'sen\' payload: '+JSON.stringify(payload));
+  socket.emit('send_message',payload);
 }
 
-$('#messages').append('<h4>'+username+'</h4>');
+socket.on('send_message_response', function(payload){
+  if(payload.result == 'fail') {
+    alert(payload.message);
+    return;
+  }
+  $('#messages').append('<p><b>'+payload.username+' says:<b> '+payload.message+'</p>');
+});
+
+$(function(){
+var payload = {};
+payload.room = chat_room;
+payload.username = username;
+console.log('*** Client Log Message: \'join_room\' payload: '+JSON.stringify(payload));
+  socket.emit('join_room',payload);
+});
