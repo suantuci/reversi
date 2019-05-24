c62bb424ca96c2caafd06a78570bcf5035758fb6
Tara Suan <suant@uci.edu>
2019-05-23 21:48:54 -0700

    errors

--- public/js/main.js
+++ public/js/main.js
@@ -31,7 +31,8 @@ socket.on('log', function(array){
 
 /* what to do when the server responds that someone joined a room*/
 socket.on('join_room_response', function(payload){
-  console.log(payload);
+  console.log('poop');
+  console.log(payload.result)
   if(payload.result == 'fail') {
     alert(payload.message);
     return;
