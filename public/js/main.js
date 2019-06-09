b6f1747a76ab1036d591caadf1dc484819b6d9e4
Tara Suan <suant@uci.edu>
2019-06-09 09:19:38 -0700

    legal moves flipping update game over logic

--- public/js/main.js
+++ public/js/main.js
@@ -31,7 +31,7 @@ socket.on('log', function(array){
 
 /* what to do when the server responds that someone joined a room*/
 socket.on('join_room_response', function(payload){
-  console.log('poop');
+
   if(payload.result == 'fail') {
     alert(payload.message);
     return;
@@ -277,6 +277,7 @@ var old_board = [
 ];
 
 var my_color = ' ';
+var interval_timer;
 
 socket.on('game_update', function(payload){
     console.log('*** Client Log Message: \'game_update\'\n\tpayload: '+JSON.stringify(payload));
@@ -307,7 +308,25 @@ socket.on('game_update', function(payload){
     }
 
     $('#my_color').html('<h3 id="my_color">I am '+my_color+'</h3>')
+    $('#my_color').append('<h4>It is '+payload.game.whose_turn+'\'s turn. Elapsed time <span id="elapsed"></span></h4>');
+    clearInterval(interval_timer);
+    interval_timer = setInterval(function(last_time){
+      return function(){
+        //do the work of updating the ui
 
+      var d = new Date();
+      var elapsedmilli = d.getTime() - last_time;
+      var minutes = Math.floor(elapsedmilli / (60 * 1000));
+      var seconds = Math.floor((elapsedmilli % (60 * 1000))/ 1000);
+
+      if(seconds <10){
+        $('#elapsed').html(minutes+':0'+seconds);
+      }
+      else{
+        $('#elapsed').html(minutes+':'+seconds);
+      }
+      }}(payload.game.last_move_time)
+      ,1000);
     /* animate changes to the board */
 
     var blacksum = 0;
@@ -358,29 +377,30 @@ socket.on('game_update', function(payload){
           else {
               $('#'+row+'_'+column).html('<img src="assets/images/error.gif" alt="error"/>');
           }
-          /* set up interaction */
-          $('#'+row+'_'+column).off('click');
-          if(board[row][column] == ' '){
-              $('#'+row+'_'+column).addClass('hovered_over');
-              $('#'+row+'_'+column).click(function(r,c){
-                return function(){
-                    var payload = {};
-                    payload.row = r;
-                    payload.column = c;
-                    payload.color = my_color;
-                    console.log('*** Client log message: \'play token\' payload: '+JSON.stringify(payload));
-                    socket.emit('play_token',payload);
-                };
-              }(row,column));         
-          }
+        }
 
-          else {
-              $('#'+row+'_'+column).removeClass('hovered_over');
-          }
+
+        /* set up interaction */
+        $('#'+row+'_'+column).off('click');
+        $('#'+row+'_'+column).removeClass('hovered_over');
+
+        if(payload.game.whose_turn === my_color){
+          if(payload.game.legal_moves[row][column] === my_color.substr(0,1)){
+            $('#'+row+'_'+column).addClass('hovered_over');
+            $('#'+row+'_'+column).click(function(r,c){
+              return function(){
+                var payload = {};
+                payload.row = r;
+                payload.column = c;
+                payload.color = my_color;
+                console.log('*** Client Log Message: \'play_token\' payload: '+JSON.stringify(payload));
+                socket.emit('play_token',payload);
+            };
+          }(row,column));
         }
       }
     }
-
+  }
   $('#blacksum').html(blacksum);
   $('#whitesum').html(whitesum);  
 
